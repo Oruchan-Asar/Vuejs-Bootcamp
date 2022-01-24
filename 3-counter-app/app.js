63d31efeea19f7eee32f7e8c2910c3227db4f9e6
Oruchan Asar <oruchan.asar@gmail.com>
2022-01-24 22:28:49 +0300

    Test computed and watch

--- 3-counter-app/app.js
+++ 3-counter-app/app.js
@@ -2,14 +2,26 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      counter2: 0,
     };
   },
-  methods: {
-    incCounter() {
-      this.counter += 1;
+  methods: {},
+  computed: {
+    setCondition() {
+      console.log("condition 1 run!");
+      return this.counter > 5 ? "big" : "small";
     },
-    decCounter() {
-      this.counter -= 1;
+    setCondition2() {
+      console.log("condition 2 run!");
+      return this.counter2 > 5 ? "big" : "small";
+    },
+  },
+  watch: {
+    counter(newValue, oldValue) {
+      console.log(oldValue + "=>" + newValue);
+    },
+    setCondition(newValue, oldValue) {
+      console.log(oldValue + "=>" + newValue);
     },
   },
 }).mount("#app");
